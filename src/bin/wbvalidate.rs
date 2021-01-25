c1523d3452b8456fa365881543c4045e7efd472f
Travis Brown <travisrobertbrown@gmail.com>
2021-01-25 22:08:55 +0100

    Add Wayback digest validation tools

--- src/bin/wbvalidate.rs
+++ src/bin/wbvalidate.rs
@@ -1,67 +1,89 @@
-use cancel_culture::{cli, wayback::Store};
+use cancel_culture::{
+    cli,
+    wayback::{cdx::Client, Store},
+};
+use clap::{crate_authors, crate_version, Clap};
+use flate2::{Compression, GzBuilder};
 use std::fs::File;
+use std::io::Write;
+use std::ops::Deref;
 use std::path::Path;
 
 type Void = Result<(), Box<dyn std::error::Error>>;
 
 #[tokio::main]
 async fn main() -> Void {
-    let args: Vec<String> = std::env::args().collect();
-    let _ = cli::init_logging(3);
-    let path = Path::new(&args[1]);
-    let store = Store::load("wayback")?;
+    let opts: Opts = Opts::parse();
+    let _ = cli::init_logging(opts.verbose);
 
-    if path.is_file() {
-        let mut file = File::open(path)?;
-        let hash = Store::compute_digest_gz(&mut file)?;
-        log::info!("{}", hash);
-    } else {
-        let contents = std::fs::read_dir(path)?;
-        let mut count = 0;
-        let mut failed = 0;
-        let mut failed302 = 0;
+    let store = Store::load(opts.store_dir)?;
+    let query = opts.query;
+    let mut bad_count = 0usize;
+    let mut missing_count = 0usize;
+    let mut bad_items = vec![];
 
-        for result in contents {
-            let path = result?.path();
+    let invalid_digest_items = store
+        .invalid_digest_items(
+            |item| {
+                item.url.contains(&query)
+                    && item.status != Some(302)
+                    && item.digest != "3I42H3S6NNFQ2MSVX7XZKYAYSCX5QBYJ"
+            },
+            24,
+        )
+        .await?;
 
-            if path.is_file() {
-                let mut file = File::open(&path)?;
-                let hash = Store::compute_digest_gz(&mut file)?;
-                let stem = path
-                    .file_stem()
-                    .and_then(|oss| oss.to_str())
-                    .expect("Unexpected filename");
-                if hash == stem {
-                    count += 1;
-                } else {
-                    log::error!("Bad file: {} (expected {})", stem, hash);
-                    let items = store.items_by_digest(stem).await;
+    for (item, broken) in invalid_digest_items {
+        if !broken {
+            log::info!("Invalid hash, expected {} for {}", item.digest, item.url);
+            bad_count += 1;
+            bad_items.push(item.clone());
+        } else {
+            log::error!("Missing or broken file {} for {}", item.digest, item.url);
+            missing_count += 1;
+            bad_items.push(item.clone());
+        }
+    }
 
-                    if items.iter().filter(|item| item.status == Some(302)).count() > 0 {
-                        failed302 += 1;
-                    }
+    log::warn!("bad: {}, missing: {}", bad_count, missing_count);
 
-                    for item in items {
-                        if item.status != Some(302) {
-                            log::error!("    {:?}", item);
-                        }
-                    }
+    if !bad_items.is_empty() && opts.fix {
+        let client = Client::new();
+        for item in bad_items {
+            let result = client.download(&item, true).await?;
+            let actual = Store::compute_digest(&mut result.clone().deref())?;
+            log::info!("Downloaded {} for {} for {}", actual, item.digest, item.url);
 
-                    failed += 1;
-                }
+            if actual == item.digest {
+                let path = Path::new(&opts.tmp_dir);
+                log::info!("Saving {} to {:?}", actual, path);
+                let file = File::create(path.join(format!("{}.gz", actual)))?;
+                let mut gz = GzBuilder::new()
+                    .filename(item.infer_filename())
+                    .write(file, Compression::default());
+                gz.write_all(&result)?;
+                gz.finish()?;
             }
         }
-
-        if failed > 0 {
-            log::error!(
-                "Failed to validate {} files ({} redirects)",
-                failed,
-                failed302
-            );
-        }
-
-        log::info!("Validated {} files", count);
     }
 
     Ok(())
+}
+
+#[derive(Clap)]
+#[clap(name = "wbvalidate", version = crate_version!(), author = crate_authors!())]
+struct Opts {
+    /// Wayback Machine store directory
+    #[clap(short, long, default_value = "wayback")]
+    store_dir: String,
+    /// Temporary store data directory
+    #[clap(short, long, default_value = "tmp-store-data")]
+    tmp_dir: String,
+    /// Attempt to re-download currently invalid items
+    #[clap(short, long)]
+    fix: bool,
+    /// Level of verbosity
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+    query: String,
 }
